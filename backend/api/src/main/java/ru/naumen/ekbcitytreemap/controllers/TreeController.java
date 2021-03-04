fab122a7e2ac21c29f71c5a3cecaa0be8be20d4e
Nagovitsin Maxim <lmakcl1405@gmail.com>
2021-03-04 15:12:20 +0500

    init api project

---
+++ backend/api/src/main/java/ru/naumen/ekbcitytreemap/controllers/TreeController.java
@@ -0,0 +1,17 @@
+package ru.naumen.ekbcitytreemap.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/tree")
+public class TreeController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+
+}
---
+++ backend/api/src/test/java/ru/naumen/ekbcitytreemap/ApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.naumen.ekbcitytreemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
