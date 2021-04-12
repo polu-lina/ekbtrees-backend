be99530831c376c6a4910cc9358140501a5afa06
AnnaEleeva <anna_eleeva@yahoo.com>
2021-04-12 22:51:15 +0500

    add Provider and Role

---
+++ backend/auth/src/main/java/ru/naumen/ectmauth/user/Provider.java
@@ -0,0 +1,5 @@
+package ru.naumen.ectmauth.user;
+
+public enum Provider {
+	LOCAL, FACEBOOK, VK
+}
--- backend/auth/src/main/java/ru/naumen/ectmauth/user/User.java
+++ backend/auth/src/main/java/ru/naumen/ectmauth/user/User.java
@@ -2,26 +2,38 @@ package ru.naumen.ectmauth.user;
 
 
 import java.util.Date;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
+@Table(name="users")
 public class User {
 
     @Id
+    @Column(name="user_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
-
     @CreationTimestamp
     private Date created;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    private Integer phone;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+    private boolean enabled;
 
     public Long getUserId() {
         return userId;
@@ -63,11 +75,42 @@ public class User {
         this.password = password;
     }
 
+    public Integer getPhone() {
+        return phone;
+    }
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
     public Date getCreated() {
         return created;
     }
 
     public void setCreated(Date created) {
         this.created = created;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
     }
 }
