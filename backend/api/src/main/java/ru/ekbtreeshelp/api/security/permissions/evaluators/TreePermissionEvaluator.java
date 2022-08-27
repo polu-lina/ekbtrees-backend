bde6c8e559a4f9edf14432a9d8609917dacfacce
Maksim Nagovitsin <m.nagovitsin@tinkoff.ru>
2022-08-27 17:03:19 +0500

    make aprroved trees editable only by mods and supers

--- backend/api/src/main/java/ru/ekbtreeshelp/api/security/permissions/evaluators/TreePermissionEvaluator.java
+++ backend/api/src/main/java/ru/ekbtreeshelp/api/security/permissions/evaluators/TreePermissionEvaluator.java
@@ -3,11 +3,15 @@ package ru.ekbtreeshelp.api.security.permissions.evaluators;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import ru.ekbtreeshelp.api.security.permissions.constants.Roles;
+import ru.ekbtreeshelp.api.security.service.SecurityService;
+import ru.ekbtreeshelp.core.entity.Tree;
 import ru.ekbtreeshelp.core.repository.TreeRepository;
 import ru.ekbtreeshelp.api.security.permissions.constants.Domains;
 import ru.ekbtreeshelp.api.security.permissions.constants.Permissions;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.BiFunction;
 
 @Component(Domains.TREE + MainPermissionEvaluator.PERMISSION_EVALUATOR)
@@ -15,10 +19,11 @@ import java.util.function.BiFunction;
 public class TreePermissionEvaluator extends DomainPermissionEvaluatorBase {
 
     private final TreeRepository treeRepository;
+    private final SecurityService securityService;
 
     private final Map<String, BiFunction<Authentication, Long, Boolean>> permissionCheckers = Map.of(
-            Permissions.DELETE, this::userIsAuthor,
-            Permissions.EDIT, this::userIsAuthor
+            Permissions.DELETE, this::canEditOrDelete,
+            Permissions.EDIT, this::canEditOrDelete
     );
 
     @Override
@@ -29,5 +34,17 @@ public class TreePermissionEvaluator extends DomainPermissionEvaluatorBase {
     @Override
     protected Map<String, BiFunction<Authentication, Long, Boolean>> getPermissionCheckers() {
         return permissionCheckers;
+    }
+
+    private boolean canEditOrDelete(Authentication authentication, Long treeId) {
+        var treeOptional = treeRepository.findById(treeId);
+        if (treeOptional.isEmpty()) {
+            return false;
+        }
+        var tree = treeOptional.get();
+        if (tree.isApprovedByModerator()) {
+            return securityService.currentUserHasAnyRole(Roles.MODERATOR, Roles.SUPERUSER);
+        }
+        return userIsAuthor(authentication, treeId);
     }
 }
